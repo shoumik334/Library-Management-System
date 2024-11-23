ca040cfa09ffa99fd47a7c6d95550b863867d8a6
Sadaen Parves Shoumik <sadaen.shoumik@northsouth.edu>
2024-11-23 22:24:45 +0600

    php

--- register.php
+++ register.php
@@ -1,3 +1,52 @@
+<?php
+
+session_start();
+    include("connection.php");
+    include("functions.php");
+
+    if($_SERVER ['REQUEST_METHOD']=="POST")
+    {
+        $first_name=$_POST['first_name'];
+        $last_name=$_POST['last_name'];
+        $username=$_POST['username'];
+        $email=$_POST['email'];
+        $contactno=$_POST['contactno'];
+        $address=$_POST['address'];
+        $password=$_POST['password'];
+
+        if(!empty($first_name)
+        && !empty($last_name) 
+        && !empty($username) 
+        && !empty($email) 
+        && ! empty($contactno)
+        && !empty($address) 
+        && ! empty($password) 
+        &&!is_numeric($first_name)
+        && !is_numeric($last_name)
+        && filter_var($email,!FILTER_VALIDATE_EMAIL)
+        &&  preg_match('/^[0-9]{10,15}$/', $contactno) )
+    {
+
+        $user_id=random_num(10);
+        $query="insert into user_registration(user_id,first_name,last_name,username,email,contactno,address,password) 
+        values('$user_id','$first_name','$last_name','$username','$email','$ntactnoco','$address','$password')";
+
+        mysqli_query($query);
+        header("Location: login.php");
+        die;
+
+    }
+    else{
+        echo"Plese enter valid information";
+    }
+
+}
+?>
+
+
+
+
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -7,7 +56,7 @@
 <body>
     <div class="container">
         <h1>Registration</h1>
-
+            <form method="post">
             <label for="first_name">First Name:</label>
             <input type="text" id="first_name" name="first_name" required><br><br>
 

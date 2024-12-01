adabc364ad1dbe0bf693c42762eab9e25d0360ea
Sadaen Parves Shoumik <sadaen.shoumik@northsouth.edu>
2024-12-01 11:10:56 +0600

    9

--- home.php
+++ home.php
@@ -4,6 +4,30 @@ if(!isset($_SESSION['Login_ID'])){
     header(('location:login%20(1).php'));
 }
 
+include 'connect.php';
+
+//Fetch Total Books
+$resultBooks = $conn ->query("SELECT COUNT(*) as totalBooks FROM books");
+$rowBooks= $resultBooks -> fetch_assoc();
+$totalBooks= $rowBooks['totalBooks'];
+
+$resultMembers = $conn->query("SELECT COUNT(*) as totalMembers FROM readers");
+$rowMembers = $resultMembers->fetch_assoc();
+$totalMembers = $rowMembers['totalMembers'];
+
+$resultIssued = $conn->query("SELECT COUNT(*) as totalIssued FROM reserve");
+$rowIssued = $resultIssued->fetch_assoc();
+$totalIssued = $rowIssued['totalIssued'];
+
+$resultPublications = $conn->query("SELECT COUNT(*) as totalPublications FROM publisher");
+$rowPublications = $resultPublications->fetch_assoc();
+$totalPublications = $rowPublications['totalPublications'];
+
+$resultCategories = $conn->query("SELECT COUNT(DISTINCT Category) as totalCategories FROM books");
+$rowCategories = $resultCategories->fetch_assoc();
+$totalCategories = $rowCategories['totalCategories'];
+
+
 ?>
 
 <!DOCTYPE html>
@@ -34,19 +58,19 @@ if(!isset($_SESSION['Login_ID'])){
             <div class="stat">
                 <img src="./images/books.png" alt="">
                 <h3>Total Books</h3>
-                <p>1000</p>
+                <p><?php echo $totalBooks; ?></p>
             </div>
             <div class="stat">
                 
                 <img src="./images/profile.png" alt="">
                 <h3>Total Members</h3>
-                <p>500</p>
+                <p><?php echo $totalMembers; ?></p>
             </div>
             <div class="stat">
                 
                 <img src="./images/issued.png" alt="">
                 <h3>Issued Books</h3>
-                <p>300</p>
+                <p><?php echo $totalIssued; ?></p>
             </div>
         </div>
         <div class="stats2">
@@ -54,12 +78,12 @@ if(!isset($_SESSION['Login_ID'])){
             
                 <img src="./images/pub.png" alt="">
             <h3>Publications</h3>
-                <p>500</p>
+                <p><?php echo $totalPublications; ?></p>
             </div>
             <div class="stat2">
                 <img src="./images/categories.jpg" alt="">
                 <h3>Book Catagories</h3>
-                <p>500</p>
+                <p><?php echo $totalCategories; ?></p>
             </div>
             <div class="stat2">
                 <img src="./images/fine.png" alt="">
